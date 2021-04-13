79df0661e682c58637849e6a066b0ef59a2f2a74
sharon <martinezsharonr@gmail.com>
2021-04-12 18:01:20 -0800

    added mvc file structure, npms install, server and react app

---
+++ routes/api/index.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+//require post routes
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to the Mongo DB /need to make changes once we have a mongo database
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+
+//app running, not getting
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
