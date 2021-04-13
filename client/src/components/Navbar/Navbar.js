21a349e942f7dde3ab85ae6209bc70041e8f30bc
William Thorne <williamcthorne@gmail.com>
2021-04-12 23:06:00 -0700

    Navbar css start

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,9 +4,20 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false)
+    const[button, setButton] = useState(true)
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        }else {
+            setButton(true)
+        }
+    };
+
+    window.addEventListener('resize', showButton)
     return (
         <>
         <nav className="navbar">
@@ -54,6 +65,7 @@ function Navbar() {
                         </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
             </div>
         </nav>
         </>
