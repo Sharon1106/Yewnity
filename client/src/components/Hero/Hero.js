335dffd759d73d4b962d48cb5fdf19f541b10b5f
William Thorne <williamcthorne@gmail.com>
2021-04-15 13:05:20 -0700

    add dummy logos

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import  Button  from '../Button/Button'
 import sliderone from '../../images/Volunteer-1.jpeg'
+import slidertwo from '../../images/Volunteer-2.jpeg'
+import sliderthree from '../../images/Community-Event.jpeg'
 import Google from '../GoogleLogin/GoogleLogin';
 
 function LandingHero() {
@@ -26,7 +28,7 @@ function LandingHero() {
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://media.gettyimages.com/photos/volunteer-accepts-canned-food-donation-at-food-drive-picture-id621904294?s=612x612"
+      src={ slidertwo }
       alt="Second slide"
     />
 
@@ -38,7 +40,7 @@ function LandingHero() {
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://previews.123rf.com/images/kasto/kasto1509/kasto150900240/46290859-speakers-giving-a-talk-at-business-meeting-audience-in-the-conference-hall-business-and-entrepreneur.jpg"
+      src={ sliderthree }
       alt="Third slide"
     />
 
--- client/src/components/LandingCard/LandingCard.js
+++ client/src/components/LandingCard/LandingCard.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import LandingCardItem from './LandingCardItem'
 import './LandingCard.css'
+import orgcard1 from '../../images/Dummy-logo-1.png'
+import orgcard2 from '../../images/Dummy-logo-2.jpeg'
+import orgcard3 from '../../images/Dummy-logo-3.jpeg'
+import orgcard4 from '../../images/Dummy-logo-4.jpeg'
 
 function LandingCard() {
     return (
@@ -10,26 +14,28 @@ function LandingCard() {
                 <div className='cards-wrapper'>
                     <ul className='cards-items'>
                         <LandingCardItem
-                        src='images/Dummy-logo-1'
+                        src={ orgcard1 }
+                        size = '100px'
                         text="Test"
                         label="Org"
                         path='/org'
                         />
                         <LandingCardItem
-                        src='images/Dummy-logo-1'
-                        text="Test"
+                        src={orgcard2}
+                        size= '100px'
+                        text="Test2"
                         label="Org"
                         path='/org'
                         />
                         <LandingCardItem
-                        src='images/Dummy-logo-1'
-                        text="Test"
+                        src={orgcard3}
+                        text="Test3"
                         label="Org"
                         path='/org'
                         />
                         <LandingCardItem
-                        src='images/Dummy-logo-1'
-                        text="Test"
+                        src={orgcard4}
+                        text="Test4"
                         label="Org"
                         path='/org'
                         />
