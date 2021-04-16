c6ba4b4a89dbdc388dd325415f9ffec85314d56d
William Thorne <williamcthorne@gmail.com>
2021-04-16 13:25:06 -0700

    Org subscriptions and recent posts cards

--- client/src/views/profile.js
+++ client/src/views/profile.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import profilepic from '../images/User-Example.jpeg'
+import EventMini from '../../src/components/Event-Mini/index'
+import CardGroup from 'react-bootstrap/CardGroup'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card from 'react-bootstrap/Card'
 
 const Profile = () => {
     return (
@@ -18,13 +22,104 @@ const Profile = () => {
                 <div>
                     <h4>Guy McFly</h4>
                     <div style={{display:"flex",justifyContent:"space-between", width:"108%"}}>
-                        <h6>Something hoes here</h6>
-                        <h6>Something hoes here</h6>
-                        <h6>Something hoes here</h6>
+                        <h6>Something goes here</h6>
+                        <h6>Something goes here</h6>
+                        <h6>Something goes here</h6>
                     </div>
+                   
                 </div>
-           </div>
-           
+           </div>  
+            <div className="postHistory" style={{justifyContent:"center", borderBottom:"1px solid grey", paddingBottom:"50px"}}>
+                <h2>Recent Posts</h2>
+                <CardDeck>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional
+                            content.{' '}
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This card has even longer content than the first to
+                            show that equal height action.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                </CardDeck>
+            </div>
+            <div className="orgSubscriptions" style={{paddingTop:"30px",borderBottom:"1px solid grey", paddingBottom:"50px"}}>
+                <h3>Organizations I'm Following</h3>
+                <CardDeck>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional
+                            content.{' '}
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This card has even longer content than the first to
+                            show that equal height action.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                </CardDeck>
+
+            </div>
         </div>
         
     )
