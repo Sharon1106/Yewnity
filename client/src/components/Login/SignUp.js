daf9fc5635601ee366cb9161b2acf13d00cbad0e
sharon <martinezsharonr@gmail.com>
2021-04-20 15:14:57 -0800

    signup complete

--- client/src/components/Login/SignUp.js
+++ client/src/components/Login/SignUp.js
@@ -1,6 +1,6 @@
 import React, { Component, useEffect, useState } from "react";
 import Google from '../GoogleLogin/googlelogin';
-import { Link } from 'react-router-dom';
+import { Link, Redirect, useHistory } from 'react-router-dom';
 import { Input, SubmitBtn } from "../Form";
 import API from "../../utils/API";
 
@@ -8,12 +8,14 @@ function SignUp () {
     //name is each of the titles 
     // const url=""
     const[users, setUsers] = useState({
-        name:"",
+        email:"",
         username:"",
         password:"",
     })
+    let history = useHistory() 
 
     function handleFormSubmit(event) {
+      console.log("im here now")
         event.preventDefault()
           API.saveUser({
             email: users.email,
@@ -21,13 +23,15 @@ function SignUp () {
             password: users.password
           })
             .then(res =>{
-            console.log(res.users)
+            console.log(res)
+            history.push("/profile")
         })
       };
 
       function handleInputChange(event) {
+        console.log("Im here")
         const newuser = {...users}
-        newuser[event.target.id] = event.target.value
+        newuser[event.target.name] = event.target.value
         setUsers(newuser)
         console.log(newuser)
       };
@@ -37,29 +41,29 @@ function SignUp () {
                 <h3 className="signup text-center">Sign Up</h3>
                 {/* ---------- Component Added ~ Jeff ~ --> Can be found in components/form/ ------------ */}
                 <Input
-                name="Email Address"
+                name="email"
                 type="email"
                 placeholder="Enter Email" 
-                onchange={(event) => handleFormSubmit(event)}
+                onChange={(event) => handleInputChange(event)}
                 />
                 <Input
-                name="Username"
+                name="username"
                 type="text"
                 placeholder="Username" 
-                onchange={(event) => handleFormSubmit(event)}
+                onChange={(event) => handleInputChange(event)}
                 />
                 <Input
-                name="Password"
+                name="password"
                 type="password"
                 placeholder="Enter Password" 
-                onchange={(event) => handleFormSubmit(event)}
+                onChange={(event) => handleInputChange(event)}
                 />
                 <SubmitBtn 
                 name="Sign Up"
                 type="submit"
                 className=" my-5 text-center btn btn-primary btn-block"
                 // upon submitting user is saved to database and is taken to their profile page 
-                onchange={(event) => handleFormSubmit(event)}
+                onClick={ handleFormSubmit}
             
                 />
                 {/* ---------------------------- END JEFF ADDITIONS ---------------------------- */}
