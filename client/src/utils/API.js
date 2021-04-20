50509fa172ccb6c31288cd50fcea9353d5a9bde5
sharon <martinezsharonr@gmail.com>
2021-04-20 10:13:37 -0800

    minor changes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,14 +5,17 @@ export default {
   getUsers: function() {
     return axios.get("/api/users");
   },
+
   // Gets the user with the given id
   getUser: function(id) {
     return axios.get("/api/users/" + id);
   },
+
   // Deletes the user with the given id
   deleteUser: function(id) {
     return axios.delete("/api/users/" + id);
   },
+  
   // Saves a user to the database
   saveUser: function(userData) {
     return axios.post("/api/users", userData);
