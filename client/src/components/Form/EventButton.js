9f236b2691af4c95fa6a6a6a8cc9f71f8257cf89
Jeffery Patterson <jpatterson933@ucla.edu>
2021-04-22 10:51:31 -0700

    readded events

---
+++ client/src/components/Form/EventButton.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const STYLES = ['btn--primary', 'btn--outline']
+const SIZES = ['btn--medium', 'btn--large'];
+
+const Button = ({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) 
+    ? buttonStyle : STYLES[0]
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+    return (
+        <Link to='/sign-up' className='btn-mobile'>
+            <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize} text-center`}
+            onClick={onClick}
+            type={type}
+            >
+                {children}
+            </button>     
+        </Link>
+    )
+};
+
+export default Button;
