8b8d8cd7eb58c91a288a461f25c283182e59c373
sharon <martinezsharonr@gmail.com>
2021-04-22 12:09:32 -0800

    signuplogin on hero

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar2 from "./components/Navbar2";
-import Footer from "./components//Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import AppHome from "./views/home";
 import Profile from "./views/profile";
 import Discussions from "./views/discussions";
@@ -13,17 +13,17 @@ import MsgBoard from "./views/discussions";
 function App() {
   return (
     <>
-    <Router>
-    <Navbar2></Navbar2>
-      <Switch>
-        <Route path='/' exact component = {AppHome} /> {/*Landing Page*/}
-        <Route path='/main' exact component = { Main }  />
-        <Route path='/events' exact component = { MsgBoard }  />
-        <Route path='/forum' exact component = { Discussions }  />
-        <Route path='/profile' exact component={ Profile } />
-      </Switch>
-      <Footer></Footer>
-    </Router>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path='/' exact component={AppHome} /> {/*Landing Page*/}
+          <Route path='/main' exact component={Main} />
+          <Route path='/events' exact component={MsgBoard} />
+          <Route path='/forum' exact component={Discussions} />
+          <Route path='/profile' exact component={Profile} />
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </>
   );
 }
--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,37 +1,37 @@
 import React from "react";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Carousel from 'react-bootstrap/Carousel';
 import sliderone from '../../images/Volunteer-1.jpeg';
 import slidertwo from '../../images/Volunteer-2.jpeg';
 import sliderthree from '../../images/Community-Event.jpeg';
 import SignUpLogin from "../Login/SignUpLogin";
 
-// google, login, signup
 function LandingHero() {
-    return (    
-    <div  iv className ="heroBlock">
+  return (
+    <div iv className="heroBlock">
       <Carousel fade>
         <Carousel.Item>
-          <img className="d-block w-100" src={ sliderone } alt="First slide"/>
+          <img className="d-block w-100" src={sliderone} alt="First slide" />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             {/* user registration */}
-            <SignUpLogin/>
+            <SignUpLogin />
           </Carousel.Caption>
-      </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={ slidertwo } alt="Second slide"/>
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={ sliderthree } alt="Third slide"/>
+          <img className="d-block w-100" src={slidertwo} alt="Second slide" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <SignUpLogin />
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={sliderthree} alt="Third slide" />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <SignUpLogin />
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
