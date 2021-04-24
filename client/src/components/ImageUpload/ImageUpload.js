04b8728bc8aba9560fa683d37f915a368c04dd47
William Thorne <williamcthorne@gmail.com>
2021-04-24 12:26:53 -0700

    image update to db

--- client/src/components/ImageUpload/ImageUpload.js
+++ client/src/components/ImageUpload/ImageUpload.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import API from '../../utils/API'
 class ImageUpload extends React.Component {
   constructor(props){
      super(props);
@@ -12,6 +13,16 @@ class ImageUpload extends React.Component {
     (error, result) => {
       if (!error && result && result.event === "success") { 
       console.log(result.info.url); 
+      let id=JSON.parse(localStorage.getItem('user')).user._id;
+      console.log(id)
+      API.updateUser(id,{
+        image: result.info.url
+      } )
+        .then(res => {
+          console.log(res)
+          
+        })
+    
     }});
     widget.open()
   }
